40efd65eea09696b938913db3ff40403e343de27
cvanelteren <caspervanelteren@gmail.com>
2021-07-04 21:12:21 +0200

    removed logs; added basic cli

--- flash.py
+++ flash.py
@@ -1,7 +1,18 @@
-import subprocess
-devs = [f"/dev/ttyUSB{idx}" for idx in range(2)]
+import subprocess, click
 
-for dev in devs:
-    print(f"Flashing {dev}")
-    subprocess.call(f"pio run -t upload --upload-port {dev}".split())
+@click.command()
+@click.option("-d", "--device", default = 'all')
+def flash_all(device):
+    devs = [f"/dev/ttyUSB{idx}" for idx in range(2)]
+    try:
+        device = f"/dev/ttyUSB{int(d)}"
+    except:
+        print("Not an integer")
+    if device != 'all':
+        devs = [device]
+    for dev in devs:
+        print(f"Flashing {dev}")
+        subprocess.call(f"pio run -t upload --upload-port {dev}".split())
+if __name__ == "__main__":
+    flash_all()
 

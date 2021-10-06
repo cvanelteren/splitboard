4f99a1d6cb0257c2f1b69cb1b537439b9885a2ca
cvanelteren <caspervanelteren@gmail.com>
2021-10-06 11:44:54 +0200

    logo init

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -38,7 +38,7 @@ Keyboard::Keyboard(Config *config) {
                     {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,
                      KC_O, KC_P, KC_LCBRACKET, KC_RCBRACKET},
                     {KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,
-                     KC_L, KC_COLON, KC_SQUOTE},
+                     KC_L, KC_SCOLON, KC_SQUOTE},
                     {KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,
                      KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT},
                     {KC_NO, KC_NO, KC_NO, KC_ALT, KC_LCTL, KC_SPC,
@@ -165,19 +165,6 @@ void Keyboard::process_keyswitch(keyswitch_t &keyswitch, bool add_special = 0) {
    * @return     return type
    */
   static uint8_t toggle_active_layer; // remember the past active layer
-  // encodes pin switch to actual char
-
-  // Serial.printf("row %d \t col %d (%d, %d)\t %d \n", keyswitch.row,
-  //               keyswitch.col, keyswitch.source, keyswitch.sinc,
-  //               keyswitch.time);
-  // if (keyswitch.pressed_down) {
-  //   Serial.println("Key press");
-  // }
-  // // release event
-  // else {
-  //   Serial.println("Key release");
-  // }
-
   uint16_t keycode = (*active_layer)[keyswitch.col][keyswitch.row];
   bool add_special_key;
   uint16_t layer;
@@ -276,16 +263,6 @@ void Keyboard::process_keyswitch(keyswitch_t &keyswitch, bool add_special = 0) {
       break;
     }
     break;
-
-    // if (this->bluetooth.isConnected()) {
-    //   if (keyswitch.pressed_down) {
-    //     Serial.printf("BLE send\n");
-    //     this->bluetooth.press(keycode);
-    //   } else {
-    //     Serial.printf("BLE release\n");
-    //     this->bluetooth.release(keycode);
-    //   }
-    // }
   }
   }
 }
